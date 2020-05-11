037d843288ca5cca890f56568168556ae6bafe92
Ziyan <gongziyan1@huawei.com>
2020-05-11 15:32:43 +0800

    fix distributd training tutroial and code

--- tutorials/tutorial_code/distributed_training/run.sh
+++ tutorials/tutorial_code/distributed_training/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-export RANK_TABLE_FILE=./rank_table.json
+EXEC_PATH=$(pwd)
+export MINDSPORE_HCCL_CONFIG_PATH=${EXEC_PATH}/rank_table.json
 export RANK_SIZE=8
+
 for((i=0;i<$RANK_SIZE;i++))
 do
     rm -rf device$i
     mkdir device$i
-    cp ./resnet50_distributed_training.py ./device$i
+    cp ./resnet50_distributed_training.py ./resnet.py ./device$i
     cd ./device$i
     export DEVICE_ID=$i
+    export RANK_ID=$i
     echo "start training for device $i"
     env > env$i.log
     pytest -s -v ./resnet50_distributed_training.py > train.log$i 2>&1 &
