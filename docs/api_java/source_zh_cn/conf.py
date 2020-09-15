3ba8d20bfb95d770b89aafc8f3076d8d148f3f09
JunYuLiu <you@example.com>
2020-09-15 18:24:29 +0800

    Modify the conf files

--- docs/api_java/source_zh_cn/conf.py
+++ docs/api_java/source_zh_cn/conf.py
@@ -11,8 +11,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sys
-
+# import sys
+# sys.path.append('..')
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
@@ -30,8 +31,8 @@ release = 'master'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
     'sphinx_markdown_tables',
+    'recommonmark',
 ]
 
 source_suffix = {
@@ -48,6 +49,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 pygments_style = 'sphinx'
+
 
 # -- Options for HTML output -------------------------------------------------
 
