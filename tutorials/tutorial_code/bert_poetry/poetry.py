1598db13c490ef0ea933100e292ba160088b68fa
wangjun <wangjun289@huawei.com>
2021-04-13 17:15:13 +0800

    add bert_poetry to tutorial code

---
+++ tutorials/tutorial_code/bert_poetry/poetry.py
@@ -0,0 +1,206 @@
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+'''
+Bert finetune script.
+'''
+
+import os
+import argparse
+from src.utils import BertPoetry, BertPoetryCell, BertLearningRate, BertPoetryModel
+from src.finetune_config import cfg, bert_net_cfg
+import mindspore.common.dtype as mstype
+from mindspore import context
+from mindspore import log as logger
+import mindspore.dataset as de
+import mindspore.dataset.transforms.c_transforms as C
+from mindspore.nn.wrap.loss_scale import DynamicLossScaleUpdateCell
+from mindspore.nn.optim import AdamWeightDecay, Lamb, Momentum
+from mindspore.train.model import Model
+from mindspore.train.callback import Callback
+from mindspore.train.callback import CheckpointConfig, ModelCheckpoint
+from mindspore.train.serialization import load_checkpoint, load_param_into_net
+from src.poetry_dataset import create_poetry_dataset, create_tokenizer
+from generator import generate_random_poetry, generate_hidden
+from mindspore.common.tensor import Tensor
+from mindspore.common.parameter import Parameter
+import mindspore.common.dtype as mstype
+from mindspore.train.serialization import export
+import numpy as np
+import time
+import re
+
+class LossCallBack(Callback):
+    '''
+    Monitor the loss in training.
+    If the loss is NAN or INF, terminate training.
+    Note:
+        If per_print_times is 0, do not print loss.
+    Args:
+        per_print_times (int): Print loss every times. Default: 1.
+    '''
+    def __init__(self, model, per_print_times=1):
+        super(LossCallBack, self).__init__()
+        if not isinstance(per_print_times, int) or per_print_times < 0:
+            raise ValueError("print_step must be in and >= 0.")
+        self._per_print_times = per_print_times
+        self.model = model
+
+    def step_end(self, run_context):
+        cb_params = run_context.original_args()
+        with open("./loss.log", "a+") as f:
+            f.write("epoch: {}, step: {}, outputs are {}".format(cb_params.cur_epoch_num, cb_params.cur_step_num,
+                                                                 str(cb_params.net_outputs)))
+            f.write("\n")
+
+
+def test_train():
+    '''
+    finetune function
+    '''
+    target = args_opt.device_target
+    if target == "Ascend":
+        devid = int(os.getenv('DEVICE_ID'))
+        context.set_context(mode=context.GRAPH_MODE, device_target="Ascend", device_id=devid)
+
+    poetry, tokenizer, keep_words = create_tokenizer()
+    print(len(keep_words))
+
+    dataset = create_poetry_dataset(bert_net_cfg.batch_size, poetry, tokenizer)
+
+    num_tokens = 3191
+    poetrymodel = BertPoetryModel(bert_net_cfg, True, num_tokens, dropout_prob=0.1)
+    netwithloss = BertPoetry(poetrymodel, bert_net_cfg, True, dropout_prob=0.1)
+    callback = LossCallBack(poetrymodel)
+
+    # optimizer
+    steps_per_epoch = dataset.get_dataset_size()
+    print("============ steps_per_epoch is {}".format(steps_per_epoch))
+    lr_schedule = BertLearningRate(learning_rate=cfg.AdamWeightDecay.learning_rate,
+                                       end_learning_rate=cfg.AdamWeightDecay.end_learning_rate,
+                                       warmup_steps=1000,
+                                       decay_steps=cfg.epoch_num*steps_per_epoch,
+                                       power=cfg.AdamWeightDecay.power)
+    optimizer = AdamWeightDecay(netwithloss.trainable_params(), lr_schedule)
+    # load checkpoint into network
+    ckpt_config = CheckpointConfig(save_checkpoint_steps=steps_per_epoch, keep_checkpoint_max=1)
+    ckpoint_cb = ModelCheckpoint(prefix=cfg.ckpt_prefix, directory=cfg.ckpt_dir, config=ckpt_config)
+
+    param_dict = load_checkpoint(cfg.pre_training_ckpt)
+    new_dict = {}
+
+    
+
+    # load corresponding rows of embedding_lookup
+    for key in param_dict:
+        if "bert_embedding_lookup" not in key:
+            new_dict[key] = param_dict[key]
+        else:
+            value = param_dict[key]
+            np_value = value.data.asnumpy()
+            np_value = np_value[keep_words]
+            tensor_value = Tensor(np_value, mstype.float32)
+            parameter_value = Parameter(tensor_value, name=key)
+            new_dict[key] = parameter_value
+
+    load_param_into_net(netwithloss, new_dict)
+    update_cell = DynamicLossScaleUpdateCell(loss_scale_value=2**32, scale_factor=2, scale_window=1000)
+    netwithgrads = BertPoetryCell(netwithloss, optimizer=optimizer, scale_update_cell=update_cell)
+
+    model = Model(netwithgrads)
+    model.train(cfg.epoch_num, dataset, callbacks=[callback, ckpoint_cb], dataset_sink_mode=True)
+
+def test_eval(ckpt_path):
+    target = args_opt.device_target
+    if target == "Ascend":
+        devid = int(os.getenv('DEVICE_ID'))
+        context.set_context(mode=context.GRAPH_MODE, device_target="Ascend", device_id=devid)
+    bert_net_cfg.batch_size = 1
+    poetrymodel = BertPoetryModel(bert_net_cfg, False, 3191, dropout_prob=0.0)
+    poetrymodel.set_train(False)
+    param_dict = load_checkpoint(ckpt_path)
+    load_param_into_net(poetrymodel, param_dict)
+    
+    #random generation/continue 
+    start_time = time.time()
+    output = generate_random_poetry(poetrymodel, s='')
+    end_to_end_delay = (time.time()-start_time)*1000
+    a = re.findall(r'[\u4e00-\u9fa5]*[\uff0c\u3002]', output)
+      
+    print("\n**********************************")
+    print("随机生成: \n")
+    for poem in a:
+      print(poem)
+    print("\ncost time: {:.1f} ms".format(end_to_end_delay))
+    print("\n")
+
+    start = "天下为公"
+    start_time = time.time()
+    output = generate_random_poetry(poetrymodel, s=start)
+    end_to_end_delay = (time.time()-start_time)*1000
+    a = re.findall(r'[\u4e00-\u9fa5]*[\uff0c\u3002]', output)
+
+    print("\n**********************************")
+    print("续写 【{}】: \n".format(start))
+    for poem in a:
+      print(poem)
+    print("\ncost time: {:.1f} ms".format(end_to_end_delay))
+    print("\n")
+
+
+
+    #hidden poetry
+    s="人工智能"
+    start_time = time.time()
+    output = generate_hidden(poetrymodel, head=s)
+    end_to_end_delay = (time.time()-start_time)*1000
+    a = re.findall(r'[\u4e00-\u9fa5]*[\uff0c\u3002]', output)
+    print("\n**********************************")
+    print("藏头诗 【{}】: \n".format(s))
+    for poem in a:
+      print(poem)
+    print("\ncost time: {:.1f} ms".format(end_to_end_delay))
+    print("\n")
+
+
+def export_net(ckpt_path):
+    bert_net_cfg.batch_size = 1
+    poetrymodel = BertPoetryModel(bert_net_cfg, False, 3191, dropout_prob=0.0)
+    poetrymodel.set_train(False)
+    param_dict = load_checkpoint(ckpt_path)
+    load_param_into_net(poetrymodel, param_dict)
+    input_id = np.ones(shape=(1,128))
+    token_type_id = np.ones(shape=(1,128))
+    pad_mask = np.ones(shape=(1,128))
+    export(poetrymodel, Tensor(input_id, mstype.int32), Tensor(token_type_id, mstype.int32), Tensor(pad_mask, mstype.float32),\
+            file_name='poetry.pb', file_format='MINDIR')
+
+parser = argparse.ArgumentParser(description='Bert finetune')
+parser.add_argument('--device_target', type=str, default='Ascend', help='Device target')
+parser.add_argument('--train', type=str, default="True", help='train or inference')
+parser.add_argument('--ckpt_path', type=str, help='path of your ckpt')
+parser.add_argument('--export', type=str, default="False", help="whether export MINDIF")
+args_opt = parser.parse_args()
+if __name__ == "__main__":
+    if args_opt.export in ["true", "True", "TRUE"]:
+        ckpt_path = args_opt.ckpt_path
+        export_net(ckpt_path)
+        exit()
+
+    if args_opt.train in ["true", "True", "TRUE"]:
+        test_train()
+    else:
+        ckpt_path = args_opt.ckpt_path
+        test_eval(ckpt_path)
---
+++ tutorials/tutorial_code/bert_poetry/src/poetry_dataset.py
@@ -0,0 +1,112 @@
+import numpy as np
+from collections import defaultdict
+import mindspore.dataset as de
+import mindspore.dataset.transforms.c_transforms as C
+import mindspore.common.dtype as mstype
+from .finetune_config import cfg
+from .poetry_utils import Tokenizer
+
+def load_vocab(vacab_path):
+    token_to_id = {}
+    with open(vacab_path) as f:
+        lines = f.readlines()
+    for i, line in enumerate(lines):
+        token = line.strip()
+        token_to_id[token] = i
+    return token_to_id
+
+def create_tokenizer(length=128):
+    dict_path = cfg.dict_path
+    forbidden_words = cfg.disallowed_words
+    max_len = cfg.max_len
+    frequency_threshold = cfg.min_word_frequency
+    batch_size = cfg.batch_size
+
+    poetry_src = []
+    with open(cfg.dataset_path) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if line.count(':') != 1:
+            continue
+        poem = line.split(':')
+        if len(poem) > 2:
+            continue
+        poem = poem[1]
+        forbidden_poem = [word in poem for word in forbidden_words]
+        if sum(forbidden_poem) > 0 or len(poem) > max_len-2:
+            continue
+        poetry_src.append(poem)
+
+    token_to_id = load_vocab(dict_path)
+    _tokenizer = Tokenizer(token_to_id, do_lower_case=True)
+
+    token_num_dict = defaultdict(int)
+    for poem in poetry_src:
+        for token in _tokenizer.tokenize(poem):
+            token_num_dict[token] += 1
+
+
+    kept_token = []
+    for token, num in token_num_dict.items():
+        if num < int(frequency_threshold):
+            continue
+        kept_token.append((token, num))
+
+    kept_token = [token for token, _ in sorted(kept_token, key=lambda x: -x[1])]
+
+    kept_token_id = []
+    tokens_id_dict = {}
+    for i, token in enumerate(['[PAD]', '[UNK]', '[CLS]', '[SEP]']):
+        tokens_id_dict[token] = i
+        kept_token_id.append(token_to_id[token])
+    
+
+    for i, token in enumerate(kept_token):
+        if token in token_to_id and token not in tokens_id_dict:
+            tokens_id_dict[token] = len(tokens_id_dict)
+            kept_token_id.append(token_to_id[token])
+
+    tokenizer = Tokenizer(tokens_id_dict, do_lower_case=True)
+
+    return poetry_src, tokenizer, kept_token_id
+
+
+def padding(input, length=None):
+    input = np.array(input)
+    padding_length = length - input.shape[-1]
+    output = np.pad(input, ((0, padding_length)), 'constant', constant_values=0)
+    return output
+
+class PoetryDataGenerator(object):
+    def __init__(self, batch_size, poetry, tokenizer, length=128):
+        self.data = poetry
+        self.batch_size = batch_size
+        self.tokenizer = tokenizer
+        self.length = length
+
+    def __getitem__(self, index):
+        np.random.shuffle(self.data)
+        current_data = self.data[index]
+
+        token_ids, segment_ids = self.tokenizer.encode(current_data)
+        batch_token_ids = padding(token_ids, length=self.length)
+        batch_segment_ids = padding(segment_ids, length=self.length)
+        pad_mask = (batch_token_ids != 0).astype(np.float32)
+        return (batch_token_ids, batch_segment_ids, pad_mask)
+
+    def __len__(self):
+        return len(self.data)
+
+
+def create_poetry_dataset(batch_size, poetry, tokenizer):
+    dt = PoetryDataGenerator(batch_size, poetry, tokenizer)
+    ds = de.GeneratorDataset(dt, ["input_ids", "token_type_id", "pad_mask"])
+    #ds.set_dataset_size(dt.__len__())
+    int_type_cast_op = C.TypeCast(mstype.int32)
+    float_type_cast_op = C.TypeCast(mstype.float32)
+    ds = ds.map(input_columns="input_ids", operations=int_type_cast_op)
+    ds = ds.map(input_columns="token_type_id", operations=int_type_cast_op)
+    ds = ds.map(input_columns="pad_mask", operations=float_type_cast_op)
+    ds = ds.batch(batch_size, drop_remainder=True)
+    return ds
