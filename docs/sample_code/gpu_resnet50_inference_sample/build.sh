bc18ee277f31f915e038ed9571802700f34a44b7
wilfChen <chenweifeng720@huawei.com>
2021-08-02 19:10:21 +0800

    gpu cpp inference tutorials

---
+++ docs/sample_code/gpu_resnet50_inference_sample/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash                                                                                                                                                                                                
+if [ -d out ]; then
+    rm -rf out
+fi
+
+mkdir out
+cd out || exit
+
+if [ -f "Makefile" ]; then
+  make clean
+fi
+
+cmake .. \
+    -DMINDSPORE_PATH="`pip show mindspore-gpu | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+make
+
