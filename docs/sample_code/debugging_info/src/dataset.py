3a60bc8d2a2452e7fe897229f93bbcc94aa833a7
liutongtong <liutongtong9@huawei.com>
2021-08-04 18:01:42 +0800

    edit dataset.py

--- docs/sample_code/debugging_info/src/dataset.py
+++ docs/sample_code/debugging_info/src/dataset.py
@@ -16,86 +16,27 @@
 Custom dataset.
 """
 import numpy as np
-from mindspore import Tensor
+from mindspore import dataset as ds
 
-
-def create_train_dataset(image_size=(1, 32, 32), num_classes=10):
-    """train dataset."""
-    ds = CustomDataSet(image_size=image_size, num_classes=num_classes)
-    return ds
-
-
-def create_eval_dataset(image_size=(1, 32, 32), num_classes=10):
-    """eval dataset"""
-    ds = CustomDataSet(size=2048, batch_size=2048, image_size=image_size, num_classes=num_classes)
-    return ds
-
-
-class CustomDataSet:
-    """CustomDataset"""
-    def __init__(self, size=32768, batch_size=32, image_size=(1, 32, 32), num_classes=10, is_onehot=True):
-        """init"""
-        self.size = size
-        self.batch_size = batch_size
-        self.image_size = image_size
-        self.num_classes = num_classes
-        self.batch_index = 0
-        self.is_onehot = is_onehot
-        self.repeat_count = 1
-        self.batch_data_size = (self.batch_size,) + image_size
-
-    def get_batch_size(self):
-        """get batch size"""
-        return self.batch_size
-
-    def get_dataset_size(self):
-        """get dataset size"""
-        return int(self.size / self.batch_size)
-
-    def get_repeat_count(self):
-        """get repeat count"""
-        return self.repeat_count
-
-    def create_tuple_iterator(self, num_epochs=-1, do_copy=False):
-        """create tuple iterator"""
-        self.num_epochs = num_epochs
-        self.do_copy = do_copy
-        return self
-
-    def __getitem__(self, batch_index):
-        """get item"""
-        if batch_index * self.batch_size >= len(self):
-            raise IndexError("{} index out of range".format(self.__class__.__name__))
-        rng_state = np.random.get_state()
-        np.random.seed(batch_index)
-        img = np.random.randn(*self.batch_data_size)
-        target = np.random.randint(0, self.num_classes, size=(1, self.batch_size))
-        np.random.set_state(rng_state)
-        img_ret = img.astype(np.float32)
-        target_ret = target.astype(np.float32)
-        if self.is_onehot:
-            target_onehot = np.zeros(shape=(self.batch_size, self.num_classes))
-            target_onehot[np.arange(self.batch_size), target] = 1
+def get_data(num, img_size=(1, 32, 32), num_classes=10, is_onehot=True):
+    for _ in range(num):
+        img = np.random.randn(*img_size)
+        target = np.random.randint(0, num_classes)
+        target_ret = np.array([target]).astype(np.float32)
+        if is_onehot:
+            target_onehot = np.zeros(shape=(num_classes,))
+            target_onehot[target] = 1
             target_ret = target_onehot.astype(np.float32)
-        return Tensor(img_ret), Tensor(target_ret)
+        yield img.astype(np.float32), target_ret
 
-    def __len__(self):
-        """get size"""
-        return self.size
+def create_train_dataset(num_data=32768, batch_size=32, repeat_size=1):
+    input_data = ds.GeneratorDataset(list(get_data(num_data)), column_names=['data', 'label'])
+    input_data = input_data.batch(batch_size, drop_remainder=True)
+    input_data = input_data.repeat(repeat_size)
+    return input_data
 
-    def __iter__(self):
-        """iter dataset"""
-        self.batch_index = 0
-        return self
-
-    def reset(self):
-        """reset dataset"""
-        self.batch_index = 0
-
-    def __next__(self):
-        """get next batch"""
-        if self.batch_index * self.batch_size < len(self):
-            data = self[self.batch_index]
-            self.batch_index += 1
-            return data
-        raise StopIteration
+def create_eval_dataset(num_data=2048, batch_size=2048, repeat_size=1):
+    input_data = ds.GeneratorDataset(list(get_data(num_data)), column_names=['data', 'label'])
+    input_data = input_data.batch(batch_size)
+    input_data = input_data.repeat(repeat_size)
+    return input_data
