588ed1cedbf3f81710b7a3dffd9f0c9cb7cc79b2
lvmingfu <lvmingfu@huawei.com>
2021-08-20 09:39:00 +0800

    fix custom_debugging_info.py for r1.3

--- docs/sample_code/distributed_export/dataset.py
+++ docs/sample_code/distributed_export/dataset.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-'''dataset
-The sample can be run on Ascend 910 AI processor.
+'''
+Dataset
 '''
 import numpy as np
 from mindspore import Tensor
