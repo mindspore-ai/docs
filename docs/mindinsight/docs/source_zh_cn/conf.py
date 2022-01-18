4e8cf8205ead1f11fffe66941e76898dfb9d8a93
zhangyi <zhangyi267@huawei.com>
2022-01-18 20:05:04 +0800

    add config for anchor.

--- docs/mindinsight/docs/source_zh_cn/conf.py
+++ docs/mindinsight/docs/source_zh_cn/conf.py
@@ -14,7 +14,6 @@ import os
 import IPython
 import re
 import sys
-import nbsphinx as nbs
 
 from sphinx.ext import autodoc as sphinx_autodoc
 
@@ -85,20 +84,9 @@ intersphinx_mapping = {
     'numpy': ('https://docs.scipy.org/doc/numpy/', '../../../../resource/numpy_objects.inv'),
 }
 
-# Remove extra outputs for nbsphinx extension.
-nbsphinx_source_re = re.compile(r"(app\.connect\('html-collect-pages', html_collect_pages\))")
-nbsphinx_math_re = re.compile(r"(\S.*$)")
-mod_path = os.path.abspath(nbs.__file__)
-with open(mod_path, "r+", encoding="utf8") as f:
-    contents = f.readlines()
-    for num, line in enumerate(contents):
-        _content_re = nbsphinx_source_re.search(line)
-        if _content_re and "#" not in line:
-            contents[num] = nbsphinx_source_re.sub(r"# \g<1>", line)
-        if "mathjax_config = app.config" in line and "#" not in line:
-            contents[num:num+10] = [nbsphinx_math_re.sub(r"# \g<1>", i) for i in contents[num:num+10]]
-            break
-    exec("".join(contents), nbs.__dict__)
+sys.path.append(os.path.abspath('../../../../resource/sphinx_ext'))
+import anchor_mod
+import nbsphinx_mod
 
 # Modify default signatures for autodoc.
 autodoc_source_path = os.path.abspath(sphinx_autodoc.__file__)
---
+++ resource/sphinx_ext/anchor_mod.py
@@ -0,0 +1,59 @@
+"""Modify docutils.nodes."""
+import os
+import inspect
+from docutils import nodes as docu_node
+from docutils.nodes import document
+import nbsphinx as nbs
+
+
+short_title_define = '''\
+def short_title(titlename):
+    titlename = titlename.lower()
+    cn_symbol = "。；，“”（）、？《》"
+    en_symbol = ".,=+*()<>[]{}|/"
+    spec_symbol = "_:："
+    for i in cn_symbol:
+        titlename = titlename.replace(i, '')
+    for j in en_symbol:
+        titlename = titlename.replace(j, '')
+    for k in spec_symbol:
+        titlename = titlename.replace(k, ' ')
+    titlename = titlename.replace('  ', ' ').replace(' ', '-')
+
+    return titlename
+'''
+
+dupname_rewrite = """
+def dupname(node, name):
+    node['dupnames'].append(name)
+    if node['names']:
+        node['names'].remove(name)
+    node.referenced = 1
+"""
+
+before = "        node['ids'].append(id)"
+after = """\
+        if node['names']:
+            id = node['names'][0]
+            id = short_title(id)
+        node['ids'].append(id)"""
+
+# Mod nodes for docutils.
+document_content = inspect.getsource(document)
+document_content = document_content.replace(before, after)
+#pylint: disable=exec-used
+exec(short_title_define, docu_node.__dict__)
+exec(document_content, docu_node.__dict__)
+exec(dupname_rewrite, docu_node.__dict__)
+
+# Remove extra outputs for nbsphinx extension.
+nbsphinx_source = r"    app.connect('html-collect-pages', html_collect_pages)\n"
+before2 = "link_id = title.replace(' ', '-')"
+after2 = "link_id = short_title(title)"
+mod_path = os.path.abspath(nbs.__file__)
+with open(mod_path, "r+", encoding="utf8") as f:
+    contents = f.read()
+    contents.replace(nbsphinx_source, '')
+    contents = contents.replace(before2, after2)
+    exec(short_title_define, nbs.__dict__)
+    exec(contents, nbs.__dict__)
