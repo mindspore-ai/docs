ba519691f913a2b68e56db9616da36a904c4b892
huodagu <yuhan24@h-partners.com>
2022-05-10 10:07:37 +0800

    add examples

--- docs/hub/docs/source_zh_cn/conf.py
+++ docs/hub/docs/source_zh_cn/conf.py
@@ -84,6 +84,10 @@ intersphinx_mapping = {
     'numpy': ('https://docs.scipy.org/doc/numpy/', '../../../../resource/numpy_objects.inv'),
 }
 
+from sphinx import directives
+with open('../_ext/overwriteobjectiondirective.txt', 'r', encoding="utf8") as f:
+    exec(f.read(), directives.__dict__)
+
 # Modify default signatures for autodoc.
 autodoc_source_path = os.path.abspath(sphinx_autodoc.__file__)
 autodoc_source_re = re.compile(r'stringify_signature\(.*?\)')
