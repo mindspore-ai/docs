bf584fe72030b246d37e6c41f0d68a5672a6d239
xumengjuan1 <xumengjuan1@huawei.com>
2022-08-22 11:05:55 +0800

    modify doc

--- docs/lite/api/source_en/conf.py
+++ docs/lite/api/source_en/conf.py
@@ -360,45 +360,5 @@ for file_name in fileList:
     with open(file_name, 'w', encoding='utf-8') as p:
         p.write(file_data)
 
-# for file_name in fileList:
-#     file_data = ''
-#     with open(file_name, 'r', encoding='utf-8') as f:
-#         for line in f:
-#             line = line.replace('MS_API', '')
-#             line = line.replace('MS_CORE_API', '')
-#             line = line.replace('MIND_API', '')
-#             line = line.replace('MS_DECLARE_PARENT', '')
-#             file_data += line
-#     with open(file_name, 'w', encoding='utf-8') as p:
-#         p.write(file_data)
-
-# fileList1 = []
-# for root1, dirs1, files1 in os.walk('../include/converter/'):
-#     for fileObj1 in files1:
-#         fileList1.append(os.path.join(root1, fileObj1))
-
-# for file_name1 in fileList1:
-#     file_data1 = ''
-#     with open(file_name1, 'r', encoding='utf-8') as f:
-#         for line1 in f:
-#             line1 = re.sub(r'enum class (.*) :', r'enum class \1_converter :', line1)
-#             file_data1 += line1
-#     with open(file_name1, 'w', encoding='utf-8') as p:
-#         p.write(file_data1)
-
-# fileList2 = []
-# for root2, dirs2, files2 in os.walk('../include/runtime/'):
-#     for fileObj2 in files2:
-#         fileList2.append(os.path.join(root2, fileObj2))
-
-# for file_name2 in fileList2:
-#     file_data2 = ''
-#     with open(file_name2, 'r', encoding='utf-8') as f:
-#         for line2 in f:
-#             line2 = re.sub(r'enum class (.*) :', r'enum class \1_runtime :', line2)
-#             file_data2 += line2
-#     with open(file_name2, 'w', encoding='utf-8') as p:
-#         p.write(file_data2)
-
 sys.path.append(os.path.abspath('../../../../resource/search'))
 import search_code
