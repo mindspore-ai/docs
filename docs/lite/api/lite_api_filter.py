7f4119cac9f977a6a40a31ca005d8443815289fc
lvmingfu <lvmingfu@huawei.com>
2022-09-09 10:58:37 +0800

    filter useless api link in minpspore lite files

--- docs/lite/api/lite_api_filter.py
+++ docs/lite/api/lite_api_filter.py
@@ -10,6 +10,7 @@ def lite_class_filter(file):
         content = data.decode('utf-8')
     except UnicodeDecodeError:
         content = data.decode('GBK')
+
     filename = os.path.basename(file)
     white_files = [
         "data_type.h", "format.h", "data_type_c.h", "format_c.h",
@@ -18,10 +19,25 @@ def lite_class_filter(file):
         ]
     if filename in white_files\
         or "MS_API" in content or "MIND_API" in content or "/callback/" in file:
+        content = filter_useless_api_link(content)
         print(content.replace("MS_API", "").replace("MIND_API", "")\
             .replace("MS_CORE_API", "").replace("MS_DECLARE_PARENT", ""))
     else:
         pass
+
+def filter_useless_api_link(content):
+    """filter useless apis"""
+    useless_apis = [
+        'mindspore.ops.MatMulFusion',
+        'mindspore.ops.FakeQuantWithMinMaxVarsPerChannel',
+        'mindspore.ops.NonZero',
+        'mindspore.ops.FakeQuantWithMinMaxVars'
+        ]
+    for api in useless_apis:
+        key_str = "@ref " + api + " "
+        if key_str in content:
+            content = content.replace(key_str, api+" ")
+    return content
 
 if __name__ == "__main__":
     values = sys.argv[-1]
