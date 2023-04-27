840800ab0f033bc0d9479e1187c65c462a269470
xumengjuan1 <xumengjuan1@huawei.com>
2023-04-27 11:33:12 +0800

    modify doc

--- docs/graphlearning/docs/source_en/conf.py
+++ docs/graphlearning/docs/source_en/conf.py
@@ -161,7 +161,10 @@ src_release = os.path.join(os.getenv("GL_PATH"), 'RELEASE.md')
 des_release = "./RELEASE.md"
 with open(src_release, "r", encoding="utf-8") as f:
     data = f.read()
-content = re.findall("## [\s\S\n]*", data)
+if len(re.findall("\n## (.*?)\n",data)) > 1:
+    content = re.findall("(## [\s\S\n]*?)\n## ", data)
+else:
+    content = re.findall("(## [\s\S\n]*)", data)
 #result = content[0].replace('# MindSpore', '#', 1)
 with open(des_release, "w", encoding="utf-8") as p:
     p.write("# Release Notes"+"\n\n")
