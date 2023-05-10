8de7ac36fe3fd5275f654f9f5ddd06f2148bfab7
huodagu <yuhan24@h-partners.com>
2023-05-10 18:35:12 +0800

    modify autosummary

--- docs/mindquantum/docs/_ext/myautosummary.py
+++ docs/mindquantum/docs/_ext/myautosummary.py
@@ -363,6 +363,8 @@ class MsCnAutoSummary(Autosummary):
                 if content:
                     summary_str = summary_re.findall(content)
                     if summary_str:
+                        if re.findall("[:：,，。.;；]", summary_str[0][-1]):
+                            logger.warning(f"{display_name}接口的概述格式需调整")
                         summary_str = summary_str[0] + '。'
                     else:
                         summary_str = ''
