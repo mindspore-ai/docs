a672dbd603a35ef4f3c3377dc6701b99d7a8dfb0
huodagu <yuhan24@h-partners.com>
2023-07-04 15:03:58 +0800

    level up sphinx

--- lite/docs/source_zh_cn/conf.py
+++ lite/docs/source_zh_cn/conf.py
@@ -29,8 +29,7 @@ release = 'master'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_markdown_tables',
-    'recommonmark',
+    'myst_parser',
 ]
 
 source_suffix = {
--- lite/tutorials/source_en/conf.py
+++ lite/tutorials/source_en/conf.py
@@ -30,8 +30,7 @@ release = 'master'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
-    'sphinx_markdown_tables',
+    'myst_parser',
 ]
 
 source_suffix = {
