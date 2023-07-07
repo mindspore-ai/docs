b15657f7feb67bb6505932625aa8863a8c5e1f23
huanxiaoling <3174348550@qq.com>
2023-07-07 11:56:46 +0800

    add contents in py files in 1.11

--- docs/lite/faq/source_en/conf.py
+++ docs/lite/faq/source_en/conf.py
@@ -29,6 +29,7 @@ release = 'master'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+myst_enable_extensions = ["dollarmath", "amsmath"]
 extensions = [
     'sphinx_markdown_tables',
     'myst_parser',
