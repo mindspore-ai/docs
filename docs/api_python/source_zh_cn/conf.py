5c648d4f9c2e560349db5c13cb10ed818390429b
huodagu <yuhan24@h-partners.com>
2023-07-07 12:47:24 +0800

    add myst_enable_extensions

--- docs/api_python/source_zh_cn/conf.py
+++ docs/api_python/source_zh_cn/conf.py
@@ -40,6 +40,8 @@ release = 'r1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+myst_enable_extensions = ["dollarmath", "amsmath"]
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
