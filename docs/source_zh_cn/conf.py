4a1315a4eb7f7166504096a11c0bdbf02c97af4a
huodagu <yuhan24@h-partners.com>
2023-07-07 12:41:49 +0800

    add myst_enable_extensions

--- docs/source_zh_cn/conf.py
+++ docs/source_zh_cn/conf.py
@@ -28,6 +28,8 @@ release = '0.2.0-alpha'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+myst_enable_extensions = ["dollarmath", "amsmath"]
+
 extensions = [
     'myst_parser',
 ]
