5d9f18be5c1b51d404cd8a95aea9a461c7a0ecd2
huan <3174348550@qq.com>
2023-11-01 10:37:04 +0800

    modify the copyright number 1.0

--- docs/api_python/source_en/conf.py
+++ docs/api_python/source_en/conf.py
@@ -25,7 +25,7 @@ import mindspore_hub
 # -- Project information -----------------------------------------------------
 
 project = 'MindSpore'
-copyright = '2020, MindSpore'
+copyright = '2023, MindSpore'
 author = 'MindSpore'
 
 # The full version, including alpha/beta/rc tags
