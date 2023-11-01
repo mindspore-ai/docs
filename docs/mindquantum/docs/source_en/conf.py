c853da4acab66e0147c0aa6cab8a7361083c3cb0
LuoJianing <luojianing1@huawei.com>
2023-11-01 14:59:55 +0800

    change copyright year

--- docs/mindquantum/docs/source_en/conf.py
+++ docs/mindquantum/docs/source_en/conf.py
@@ -23,7 +23,7 @@ import mindquantum
 # -- Project information -----------------------------------------------------
 
 project = 'MindSpore'
-copyright = '2022, MindSpore'
+copyright = 'MindSpore'
 author = 'MindSpore'
 
 # The full version, including alpha/beta/rc tags
