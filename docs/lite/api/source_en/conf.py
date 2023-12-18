9735849b913700cb56e5bdf979bbe038a5c00ce4
yuhan <yuhan24@h-partners.com>
2023-12-18 10:11:31 +0800

    modify format

--- docs/lite/api/source_en/conf.py
+++ docs/lite/api/source_en/conf.py
@@ -242,6 +242,18 @@ exhale_args = {
     )
 }
 
+# Fix some dl-label lack class='simple'
+from docutils.writers import _html_base
+
+with open(_html_base.__file__, "r+", encoding="utf-8") as f:
+    code_str = f.read()
+    old_str = '''        if self.is_compactable(node):
+            classes.append('simple')'''
+    new_str = '''        if classes == []:
+            classes.append('simple')'''
+    code_str = code_str.replace(old_str, new_str)
+    exec(code_str, _html_base.__dict__)
+
 # modify source code of exhale.
 exh_file = os.path.abspath(exh_graph.__file__)
 
