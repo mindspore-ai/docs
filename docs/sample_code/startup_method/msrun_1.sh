abaf0508970decdcb7a1ccc7e942dab18ad1ebdd
ZPaC <zhoupeichen@huawei.com>
2024-02-01 20:44:58 +0800

    Add msrun doc

---
+++ docs/sample_code/startup_method/msrun_1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "=========================================="
+echo "Please run the script as: "
+echo "bash msrun_1.sh"
+echo "==========================================="
+
+EXEC_PATH=$(pwd)
+if [ ! -d "${EXEC_PATH}/MNIST_Data" ]; then
+    if [ ! -f "${EXEC_PATH}/MNIST_Data.zip" ]; then
+        wget http://mindspore-website.obs.cn-north-4.myhuaweicloud.com/notebook/datasets/MNIST_Data.zip
+    fi
+    unzip MNIST_Data.zip
+fi
+export DATA_PATH=${EXEC_PATH}/MNIST_Data/train/
+
+rm -rf msrun_log
+mkdir msrun_log
+echo "start training"
+
+msrun --worker_num=8 --local_worker_num=4 --master_addr=<node_1 ip address> --master_port=8118 --node_rank=0 --log_dir=msrun_log --join=True --cluster_time_out=300 net.py
