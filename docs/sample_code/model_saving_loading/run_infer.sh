e2c14686308111c5fa86894277075daf31fef985
huan <3174348550@qq.com>
2024-04-30 11:47:53 +0800

    delete the files 2.3.0rc2

--- docs/sample_code/model_saving_loading/run_infer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "=============================================================================================================="
-echo "Please run the script as: "
-echo "bash run_infer.sh"
-echo "=============================================================================================================="
-
-mpirun -n 4 --output-filename log_output --merge-stderr-to-stdout python model_transformation_infer.py
--- docs/sample_code/model_saving_loading/run_loading.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "=============================================================================================================="
-echo "Please run the script as: "
-echo "bash run_loading.sh"
-echo "=============================================================================================================="
-
-mpirun -n 8 --output-filename log_output --merge-stderr-to-stdout python test_loading.py
