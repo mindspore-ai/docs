3d7286ac03b2d592583799b4a4f3e92d2fd89240
yuhan <yuhan24@h-partners.com>
2024-05-13 16:52:31 +0800

    modify replace urls

--- docs/mindspore/source_zh_cn/conf.py
+++ docs/mindspore/source_zh_cn/conf.py
@@ -420,12 +420,13 @@ re_view = f"\n.. image:: https://mindspore-website.obs.cn-north-4.myhuaweicloud.
 
 for cur, _, files in os.walk(des_sir):
     for i in files:
-        if i.endswith('.rst'):
+        if i.endswith('.rst') or i.endswith('.md') or i.endswith('.ipynb'):
             try:
                 with open(os.path.join(cur, i), 'r+', encoding='utf-8') as f:
                     content = f.read()
                     new_content = re.sub(re_url, r'\1/r2.3.0rc2', content)
-                    new_content = re.sub(re_url2, r'\1/v2.3.0-rc2', new_content)
+                    if i.endswith('.rst'):
+                        new_content = re.sub(re_url2, r'\1/v2.3.0-rc2', new_content)
                     # if i.endswith('.md'):
                     #     md_view = f'[![查看源文件](https://mindspore-website.obs.cn-north-4.myhuaweicloud.com/website-images/{docs_branch}/resource/_static/logo_source.svg)](https://gitee.com/mindspore/{copy_repo}/blob/{branch}/' + copy_path + cur.split('api_python')[-1] + '/' + i + ')\n\n'
                     #     if 'resource/_static/logo_source' not in new_content:
