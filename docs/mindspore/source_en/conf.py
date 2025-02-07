b7d6aaffc8811ea986eb34a5b128b7a679fedca6
yuhan <yuhan24@h-partners.com>
2025-02-07 10:44:49 +0800

    fit 2.5.0 build

--- docs/mindspore/source_en/conf.py
+++ docs/mindspore/source_en/conf.py
@@ -322,7 +322,7 @@ if os.path.exists(dataset_list_path):
 def ops_interface_name():
 
     src_target_path = os.path.join(src_dir_en, 'mindspore.ops.primitive.rst')
-    with open(src_target_path,'r+',encoding='utf8') as f:
+    with open(src_target_path,'r',encoding='utf8') as f:
         content =  f.read()
     primi_list = re.findall("    (mindspore\.ops\.\w*?)\n", content)
 
@@ -331,7 +331,7 @@ def ops_interface_name():
 def mint_interface_name():
     mint_p = 'mindspore.mint.rst'
     src_target_path = os.path.join(src_dir_en, mint_p)
-    with open(src_target_path,'r+',encoding='utf8') as f:
+    with open(src_target_path,'r',encoding='utf8') as f:
         content =  f.read()
     mint_list = re.findall(r"    (mindspore\.mint\..*)\n", content+'\n')
 
@@ -393,17 +393,55 @@ for i in os.listdir(os.path.join(repo_path, 'mindspore/ops/op_def/yaml')):
             if re.findall('function:\n\s+?name: (.*)', op_content):
                 func_name_dict[re.findall('function:\n\s+?name: (.*)', op_content)[0]] = i.replace('_op.yaml', '')
 
+re_url = r"(((gitee.com/mindspore/docs)|(github.com/mindspore-ai/(mindspore|docs))|" + \
+         r"(mindspore.cn/(docs|tutorials|lite))|(obs.dualstack.cn-north-4.myhuaweicloud)|" + \
+         r"(mindspore-website.obs.cn-north-4.myhuaweicloud))[\w\d/_.-]*?)/(master)"
+
+re_url2 = r"(gitee.com/mindspore/mindspore[\w\d/_.-]*?)/(master)"
+
+re_url3 = r"(((gitee.com/mindspore/golden-stick)|(mindspore.cn/golden_stick))[\w\d/_.-]*?)/(master)"
+
+re_url4 = r"(((gitee.com/mindspore/mindquantum)|(mindspore.cn/mindquantum))[\w\d/_.-]*?)/(master)"
+
+re_url5 = r"(gitee.com/mindspore/mindformers[\w\d/_.-]*?)/(dev)"
+
+re_url6 = r"(mindspore.cn/mindformers[\w\d/_.-]*?)/(dev)"
+
 for cur, _, files in os.walk(des_sir):
     for i in files:
         if os.path.join(cur, i) in no_viewsource_list:
             continue
-        if i.endswith('.md'):
+        if i.endswith('.rst') or i.endswith('.md') or i.endswith('.ipynb'):
             with open(os.path.join(cur, i), 'r+', encoding='utf-8') as f:
                 content = f.read()
-                new_content = content
-                md_view = f'[![View Source On Gitee](https://mindspore-website.obs.cn-north-4.myhuaweicloud.com/website-images/{docs_branch}/resource/_static/logo_source_en.svg)](https://gitee.com/mindspore/{copy_repo}/blob/{branch}/' + copy_path + cur.split('api_python')[-1] + '/' + i + ')\n\n'
-                if 'resource/_static/logo_source' not in new_content:
-                    new_content = re.sub('(# .*\n\n)', r'\1'+ md_view, new_content, 1)
+                new_content = re.sub(re_url, r'\1/r2.5.0', content)
+                new_content = re.sub(re_url3, r'\1/r1.0.0', new_content)
+                new_content = re.sub(re_url4, r'\1/r0.10', new_content)
+                new_content = re.sub(re_url5, r'\1/v1.3.2', new_content)
+                new_content = re.sub(re_url6, r'\1/r1.3.2', new_content)
+                if i.endswith('.rst'):
+                    new_content = re.sub(re_url2, r'\1/v2.5.0', new_content)
+                # if i.endswith('.md'):
+                #     md_view = f'[![View Source On Gitee](https://mindspore-website.obs.cn-north-4.myhuaweicloud.com/website-images/{docs_branch}/resource/_static/logo_source_en.svg)](https://gitee.com/mindspore/{copy_repo}/blob/{branch}/' + copy_path + cur.split('api_python')[-1] + '/' + i + ')\n\n'
+                #     if 'resource/_static/logo_source' not in new_content:
+                #         new_content = re.sub('(# .*\n\n)', r'\1'+ md_view, new_content, 1)
+                if new_content != content:
+                    f.seek(0)
+                    f.truncate()
+                    f.write(new_content)
+
+base_path = os.path.dirname(os.path.dirname(sphinx.__file__))
+for cur, _, files in os.walk(os.path.join(base_path, 'mindspore')):
+    for i in files:
+        if i.endswith('.py'):
+            with open(os.path.join(cur, i), 'r+', encoding='utf-8') as f:
+                content = f.read()
+                new_content = re.sub(re_url, r'\1/r2.5.0', content)
+                new_content = re.sub(re_url2, r'\1/v2.5.0', new_content)
+                new_content = re.sub(re_url3, r'\1/r1.0.0', new_content)
+                new_content = re.sub(re_url4, r'\1/r0.10', new_content)
+                new_content = re.sub(re_url5, r'\1/v1.3.2', new_content)
+                new_content = re.sub(re_url6, r'\1/r1.3.2', new_content)
                 if new_content != content:
                     f.seek(0)
                     f.truncate()
@@ -496,5 +534,7 @@ else:
     content = content[0]
 
 with open(des_release, "w", encoding="utf-8") as p:
+    release_source = re.sub(re_url, r'\1/r2.5.0', release_source)
+    release_source = re.sub(re_url2, r'\1/v2.5.0', release_source)
     p.write("# Release Notes" + "\n\n" + release_source)
     p.write(content)
