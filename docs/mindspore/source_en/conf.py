10663f5eb4fa1b471a06cae471e08f087dee1454
yuhan <yuhan24@h-partners.com>
2025-05-06 16:42:35 +0800

    replace py file

--- docs/mindspore/source_en/conf.py
+++ docs/mindspore/source_en/conf.py
@@ -255,7 +255,9 @@ try:
     decorator_list = [("mindspore/common/dtype.py","del decorator",
                        "@enum.unique","# generate api by del decorator."),
                       ("mindspore/common/dtype.py","del class",
-                       "class QuantDtype(enum.Enum):","class QuantDtype():")]
+                       "class QuantDtype(enum.Enum):","class QuantDtype():"),
+                      ("mindspore/ops/function/math_func.py","modify err content",
+                       "print(>>> output)","print(output)")]
 
     base_path = os.path.dirname(os.path.dirname(sphinx.__file__))
     for i in decorator_list:
