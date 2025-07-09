6f79137a2a9f6238d8de122228197174acbfcdd2
yuhan <yuhan24@h-partners.com>
2025-07-09 14:28:13 +0800

    modify config for MSAdapter

---
+++ docs/msadapter/docs/source_zh_cn/conf.py
@@ -0,0 +1,110 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import glob
+import os
+import shutil
+import sys
+import IPython
+import re
+import sphinx
+
+# -- Project information -----------------------------------------------------
+
+project = 'MSAdapter'
+copyright = 'MindSpore'
+author = 'MindSpore'
+
+# The full version, including alpha/beta/rc tags
+release = 'master'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+myst_enable_extensions = ["dollarmath", "amsmath"]
+
+
+myst_heading_anchors = 5
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'myst_parser',
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+    'IPython.sphinxext.ipython_console_highlighting'
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# Add any paths that contain templates here, relative to this directory.
+# templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+mathjax_path = 'https://mindspore-website.obs.cn-north-4.myhuaweicloud.com/mathjax/MathJax-3.2.2/es5/tex-mml-chtml.js'
+
+mathjax_options = {
+    'async':'async'
+}
+
+smartquotes_action = 'De'
+
+exclude_patterns = []
+
+pygments_style = 'sphinx'
+
+autodoc_inherit_docstrings = False
+
+autosummary_generate = True
+
+autosummary_generate_overwrite = False
+
+# -- Options for HTML output -------------------------------------------------
+
+# Reconstruction of sphinx auto generated document translation.
+language = 'zh_CN'
+locale_dirs = ['../../../../resource/locale/']
+gettext_compact = False
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+html_search_language = 'zh'
+
+html_search_options = {'dict': '../../../../resource/jieba.txt'}
+
+sys.path.append(os.path.abspath('../../../../resource/sphinx_ext'))
+# import anchor_mod
+import nbsphinx_mod
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable', None),
+}
+
+sys.path.append(os.path.abspath('../../../../resource/search'))
+import search_code
