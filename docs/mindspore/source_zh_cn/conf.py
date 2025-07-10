97a307aff1b5d0c983d0183d429aecb9b529f1ae
yuhan <yuhan24@h-partners.com>
2025-07-10 15:28:40 +0800

    modify html_title config

--- docs/mindspore/source_zh_cn/conf.py
+++ docs/mindspore/source_zh_cn/conf.py
@@ -109,7 +109,7 @@ copyright = 'MindSpore'
 author = 'MindSpore'
 # language = 'cn'
 # The full version, including alpha/beta/rc tags
-release = 'master'
+release = '2.4.0'
 
 # -- General configuration ---------------------------------------------------
 
