28a66638bdc5b9d909761c10466970745b0ed8d8
yuhan <yuhan24@h-partners.com>
2025-08-11 09:22:26 +0800

    modify config

--- docs/lite/docs/source_en/conf.py
+++ docs/lite/docs/source_en/conf.py
@@ -22,7 +22,7 @@ copyright = 'MindSpore'
 author = 'MindSpore Lite'
 
 # The full version, including alpha/beta/rc tags
-release = 'master'
+release = '2.7.0'
 
 
 # -- General configuration ---------------------------------------------------
