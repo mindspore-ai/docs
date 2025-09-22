e1b95b659bf7d14c30315f081fa1338d48e92bc2
TingWang <kathy.wangting@huawei.com>
2025-09-22 17:51:34 +0800

    fix vllm-mindspore docs
    Signed-off-by: TingWang <kathy.wangting@huawei.com>

--- docs/vllm_mindspore/docs/source_zh_cn/conf.py
+++ docs/vllm_mindspore/docs/source_zh_cn/conf.py
@@ -302,16 +302,8 @@ def setup(app):
 
 src_release = "./release_notes/release_notes.md"
 des_release = "./RELEASE.md"
-with open(src_release, "r", encoding="utf-8") as f:
-    data = f.read()
-if len(re.findall("\n## (.*?)\n",data)) > 1:
-    content = re.findall("(## [\s\S\n]*?)\n## ", data)
-else:
-    content = re.findall("(## [\s\S\n]*)", data)
-
-with open(des_release, "w", encoding="utf-8") as p:
-    p.write("# Release Notes"+"\n\n")
-    p.write(content[0])
+if os.path.isfile(src_release):
+    os.rename(src_release, des_release)
 
 os.makedirs(os.path.join(present_path, "../build_zh_cn/html/"), exist_ok=True)
 shutil.copy(os.path.join(present_path, "arch.cn.png"), os.path.join(present_path, "../build_zh_cn/html/"))
