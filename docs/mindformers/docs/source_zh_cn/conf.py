1f9781f960074b74e2f6fefa6d0a87db6b461d9a
xuzhen <xuzhen8@huawei.com>
2025-10-17 09:36:29 +0800

    modify config for master

--- docs/mindformers/docs/source_zh_cn/conf.py
+++ docs/mindformers/docs/source_zh_cn/conf.py
@@ -313,6 +313,19 @@ docs_branch = [version_inf[i]['branch'] for i in range(len(version_inf)) if vers
 re_view = f"\n.. image:: https://mindspore-website.obs.cn-north-4.myhuaweicloud.com/website-images/{docs_branch}/" + \
           f"resource/_static/logo_source.svg\n    :target: https://gitee.com/mindspore/{copy_repo}/blob/{branch}/"
 
+# 发版本时这里启用
+# re_url = r"(((gitee.com/mindspore/docs)|(github.com/mindspore-ai/(mindspore|docs))|" + \
+#          r"(mindspore.cn/(docs|tutorials|lite))|(obs.dualstack.cn-north-4.myhuaweicloud)|" + \
+#          r"(mindspore-website.obs.cn-north-4.myhuaweicloud))[\w\d/_.-]*?)/(master)"
+
+# re_url2 = r"(gitee.com/mindspore/(mindspore|mindspore-lite)/[\w\d/_.-]*?)/(master)"
+
+# re_url3 = r"(((gitee.com/mindspore/golden-stick)|(mindspore.cn/golden_stick))/[\w\d/_.-]*?)/(master)"
+
+# re_url4 = r"(mindspore.cn/vllm_mindspore/[\w\d/_.-]*?)/(master)"
+
+# re_url5 = r"(((gitee.com/mindspore/mindformers)|(mindspore.cn/mindformers))[\w\d/_.-]*?)/(master)"
+
 for cur, _, files in os.walk(moment_dir):
     for i in files:
         flag_copy = 0
@@ -326,12 +339,21 @@ for cur, _, files in os.walk(moment_dir):
                     with open(os.path.join(cur, i), 'r+', encoding='utf-8') as f:
                         content = f.read()
                         new_content = content
+                        # master使用
                         if '.. include::' in content and '.. automodule::' in content:
                             continue
                         if 'autosummary::' not in content and "\n=====" in content:
                             re_view_ = re_view + copy_path + cur.split(moment_dir)[-1] + '/' + i + \
                                        '\n    :alt: 查看源文件\n\n'
                             new_content = re.sub('([=]{5,})\n', r'\1\n' + re_view_, content, 1)
+
+                        # 发版本时这里启用
+                        # new_content = re.sub(re_url, r'\1/r2.7.1', new_content)
+                        # new_content = re.sub(re_url2, r'\1/v2.7.1', new_content)
+                        # new_content = re.sub(re_url3, r'\1/r1.3.0', new_content)
+                        # new_content = re.sub(re_url4, r'\1/r0.4.0', new_content)
+                        # new_content = re.sub(re_url5, r'\1/r1.7.0', new_content)
+
                         if new_content != content:
                             f.seek(0)
                             f.truncate()
@@ -346,6 +368,7 @@ import nbsphinx_mod
 sys.path.append(os.path.abspath('../../../../resource/search'))
 import search_code
 
+# 发版本时这里启用
 # src_release = os.path.join(os.getenv("MFM_PATH"), 'RELEASE_CN.md')
 # des_release = "./RELEASE.md"
 # with open(src_release, "r", encoding="utf-8") as f:
