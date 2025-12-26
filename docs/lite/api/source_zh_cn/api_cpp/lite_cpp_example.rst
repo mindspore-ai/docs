0c1dc8db6c3953418b3304981e938b76a8f32147
huan-xiaoling <3174348550@qq.com>
2025-12-26 14:16:12 +0800

    delete redundant files

--- docs/lite/api/source_zh_cn/api_cpp/lite_cpp_example.rst
+++
@@ -1,9 +0,0 @@
-样例
-=======
-
-.. toctree::
-  :maxdepth: 1
-
-  极简Demo↗ <https://www.mindspore.cn/lite/docs/zh-CN/master/infer/quick_start_cpp.html>
-  基于JNI接口的Android应用开发↗ <https://www.mindspore.cn/lite/docs/zh-CN/master/infer/quick_start.html#编写端侧推理代码>
-  高阶用法↗ <https://www.mindspore.cn/lite/docs/zh-CN/master/infer/runtime_cpp.html>
--- docs/sample_code/high_dimension_tensor_parallel/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "=============================================================================================================="
-echo "Please run the script as: "
-echo "bash run.sh"
-echo "=============================================================================================================="
-
-msrun --worker_num=8 --local_worker_num=8 --master_port=8118 --log_dir=log_output --join=True \
---cluster_time_out=300 high_dimension_tensor_parallel.py
