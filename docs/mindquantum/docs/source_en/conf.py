4acf0e02613b8807d113202c1a029718c6f9988f
myprigitee <myprigitee@noreply.gitcode.com>
2026-03-24 14:45:32 +0800

    [docs][mindquantum] release config

--- docs/mindquantum/docs/source_en/conf.py
+++ docs/mindquantum/docs/source_en/conf.py
@@ -27,7 +27,7 @@ copyright = 'MindSpore'
 author = 'MindSpore'
 
 # The full version, including alpha/beta/rc tags
-release = 'master'
+release = '0.12'
 
 
 # -- General configuration ---------------------------------------------------
