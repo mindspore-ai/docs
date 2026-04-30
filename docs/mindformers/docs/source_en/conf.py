f8d2693ea1fda539a192dcdc0beb4eabd5c46394
myprigitee <lc23562025@163.com>
2026-04-30 11:24:01 +0800

    [docs][mindformers] fix code

--- docs/mindformers/docs/source_en/conf.py
+++ docs/mindformers/docs/source_en/conf.py
@@ -348,6 +348,38 @@ with open(os.path.join(base_path, 'mindformers/pynative/distributed/style.py'), 
     f.truncate()
     f.write(content)
 
+with open(os.path.join(base_path, 'mindformers/pynative/distributed/tensor_parallel.py'), 'r+', encoding='utf-8') as f:
+    content = f.read()
+    old_content = """def __init__(
+            self,
+            *,
+            input_layouts: Placement | None = None,
+            output_layouts: Placement | None = None,"""
+    new_content = """from typing import Union
+    def __init__(
+            self,
+            *,
+            input_layouts: Union[Placement, None] = None,
+            output_layouts: Union[Placement, None] = None,"""
+    content = content.replace(old_content, new_content)
+    f.seek(0)
+    f.truncate()
+    f.write(content)
+
+with open(os.path.join(base_path, 'hyper_parallel/core/dtensor/_mesh_layout.py'), 'r+', encoding='utf-8') as f:
+    content = f.read()
+    old_content = """@classmethod
+    def from_sizes_strides(
+        cls, sizes: tuple[int, ...], strides: tuple[int, ...] | None = None"""
+    new_content = """from typing import Tuple, Optional
+    @classmethod
+    def from_sizes_strides(
+        cls, sizes: Tuple[int, ...], strides: Optional[Tuple[int, ...]] = None"""
+    content = content.replace(old_content, new_content)
+    f.seek(0)
+    f.truncate()
+    f.write(content)
+
 import mindformers
 
 # Copy source files of chinese python api from golden-stick repository.
