b58c2734ae393feadc0910ed0dc8c44fb1cfbbb5
myprigitee <lc23562025@163.com>
2026-05-07 14:18:16 +0800

    sphinx version upgrade fix code

--- docs/mindspore/source_zh_cn/conf.py
+++ docs/mindspore/source_zh_cn/conf.py
@@ -26,10 +26,9 @@ from docutils.writers import _html_base
 
 with open(_html_base.__file__, "r", encoding="utf-8") as f:
     code_str = f.read()
-    old_str = '''        if self.is_compactable(node):
-            classes.append('simple')'''
-    new_str = '''        if classes == []:
-            classes.append('simple')'''
+    old_str = '''        classes = ['simple'] if self.is_compactable(node) else []'''
+    new_str = '''        classes = node.setdefault('classes', [])
+            classes = ['simple'] if classes == [] else []'''
     code_str = code_str.replace(old_str, new_str)
     exec(code_str, _html_base.__dict__)
 
