fc32ce4f835e995f975408ae37538e24df9e9b80
yanglingyue <yanglingyue2@h-partners.com>
2026-07-14 09:53:40 +0800

    [docs][ms]fix inherited-members

--- docs/mindspore/source_en/conf.py
+++ docs/mindspore/source_en/conf.py
@@ -116,9 +116,6 @@ extensions = [
     'IPython.sphinxext.ipython_console_highlighting'
 ]
 
-autodoc_default_options = {
-    'inherited-members': False,
-}
 
 source_suffix = {
     '.rst': 'restructuredtext',
