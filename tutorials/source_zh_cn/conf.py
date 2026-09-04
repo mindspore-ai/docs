9cea42fa1765bf310847a119ad7240591e2c12f4
yanglingyue <yanglingyue2@h-partners.com>
2026-09-04 11:32:16 +0800

    adapt tutorials underscore path

--- tutorials/source_zh_cn/conf.py
+++ tutorials/source_zh_cn/conf.py
@@ -48,9 +48,14 @@ html_title = author + ' ' + release + ' ' + project
 # ones.
 myst_enable_extensions = ["dollarmath", "amsmath"]
 
-lib_path = "/doc-lib/jquery.js"
-USE_LIB = os.path.exists(lib_path)
-html_context = {'use_lib': USE_LIB}
+jquery_path = "/doc-lib/jquery.js"
+underscore_path = "/doc-lib/underscore.js"
+USE_JQUERY = os.path.exists(jquery_path)
+USE_UNDERSCORE = os.path.exists(underscore_path)
+html_context = {
+    'use_jquery': USE_JQUERY,
+    'use_underscore': USE_UNDERSCORE
+}
 
 # 允许生成几级及以上的锚点
 myst_heading_anchors = 5
